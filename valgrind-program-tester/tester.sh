23bde94d14f5133b2139375e8ad2798ec7efba5a
Ville Seppanen <vseppane@c2r6p12.hive.fi>
2024-08-07 09:21:47 +0300

    valgrind program tester

---
+++ valgrind-program-tester/tester.sh
@@ -0,0 +1,71 @@
+#!/bin/zsh
+TESTCOUNTER=0
+LEAKSCOUNT=0
+EXECUTABLE="./push_swap"
+LOGFILE="vlog"
+ARGUMENTS=(
+ 	# bad arguments, should print "Error\n"
+	#"1 a"				# non-number
+	#"1 2 1"				# duplicate
+	#"1 2 1 1 2"			# duplicate
+	"2147483648"			# over maxint
+	"-2147483649"			# under minint
+
+	# no argument special case, should print nothing
+	#""
+	#" "
+
+	# proper arguments
+	#"0"
+	"2147483647"			#maxint
+	"-2147483648"			#minint
+	#"576"
+	#"-1 1"
+	#"1 2 3"
+	#"1 2 3 4"
+)
+
+# colors
+GRN="\033[1;32m"
+RED="\033[1;31m"
+RST="\033[0m"
+
+# check for executable
+if [ ! -f $EXECUTABLE ]; then
+	echo "⚡ ${RED}Executable $EXECUTABLE not found.${RST}"
+    exit 1
+fi
+
+# check -g compiler flag in (not 100% this works!)
+if file $EXECUTABLE | grep -q "not stripped"; then
+    echo "⚡ Executable contains debug information."
+else
+	echo "⚡ ${RED}Warning: Executable does not contain debug information.${RST}"
+fi
+echo "⚡ ${GRN}Testing begins!${RST}"
+echo ""
+
+# run tests
+for ARG in "${ARGUMENTS[@]}"
+do
+	TESTCOUNTER=$((TESTCOUNTER + 1))
+	# echo "⚡ logfile: ${LOGFILE}${TESTCOUNTER}"
+	valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=${LOGFILE}${TESTCOUNTER} $EXECUTABLE $ARG
+	if grep -q "All heap blocks were freed -- no leaks are possible" "${LOGFILE}${TESTCOUNTER}"; then
+    	echo "⚡ ${GRN}Test $TESTCOUNTER: No memory leaks detected."${RST}
+	else
+	    echo "⚡ ${RED}Test $TESTCOUNTER: Memory leaks detected!${RST}"
+		LEAKSCOUNT=$((LEAKSCOUNT + 1)) 
+	fi
+	echo "⚡ Args: $ARG"
+	echo ""
+done
+
+# report 
+if [ $((LEAKSCOUNT)) -gt 0 ]; then
+	echo "⚡ ${RED}There were $LEAKSCOUNT leaks found in $TESTCOUNT tests${RST}"
+else
+	echo "⚡ ${GRN}No leaks were found.${RST}"
+fi
+
+echo "⚡ ${TESTCOUNTER} Valgrind logs written as ${LOGFILE}[i]."
